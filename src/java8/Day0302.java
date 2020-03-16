0653e67d6c0724fd5e180a4dfbb333f95de39d94
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-16 12:51:35 +0900

    3일차 : 기본자료형의 최대값과 최소값

---
+++ src/java8/Day0302.java
@@ -0,0 +1,28 @@
+package java8;
+
+public class Day0302 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 기본 자료형은 각각을 정의하는 클래스가 존재.
+		 * byte -> Byte
+		 * short -> Short
+		 * int -> Integer
+		 * long -> Long
+		 * float -> Float
+		 * double -> Double
+		 * char -> Character
+		 * 
+		 * 각각의 클래스에는 상수로 MAX_VALUE와 MIN_VALUE가 존재.
+		 
+		 */
+		
+		System.out.println("Int형의 최대값 : " + Integer.MAX_VALUE);
+		System.out.println("Int형의 최소값 : " + Integer.MIN_VALUE);
+		
+		System.out.println("Byte형의 최대값 : " + Byte.MAX_VALUE);
+		System.out.println("Byte형의 최소값 : " + Byte.MIN_VALUE);
+	}
+
+}
