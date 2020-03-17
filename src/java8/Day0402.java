8926f2dd332faa8a6a4f104bcb44bbafaf90840d
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-17 14:03:36 +0900

    4일차 : System.in.read()

---
+++ src/java8/Day0402.java
@@ -0,0 +1,13 @@
+package java8;
+
+import java.io.IOException;
+
+public class Day0402 {
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("е╟ ют╥б.");
+		int name = System.in.read();
+		System.out.println(name);
+	
+	}
+}
