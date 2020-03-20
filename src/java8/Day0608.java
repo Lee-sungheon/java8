06241e2504fe979a40d9be49874e29fce4571519
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-20 15:48:04 +0900

    6일차 : 실습문제(1)

---
+++ src/java8/Day0608.java
@@ -0,0 +1,24 @@
+package java8;
+
+import java.util.Scanner;
+
+public class Day0608 {
+
+	public static void main(String[] args) {
+		int intyear;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("윤년을 판별한 년도 입력!");
+		intyear = sc.nextInt();
+		
+		if ((intyear%4==0 && intyear%100==0 && intyear%400==0) || (intyear%4==0 && intyear%100!=0))
+		{
+			System.out.println(intyear+"은 윤년이다.");
+		}
+		else
+		{
+			System.out.println(intyear+"은 윤년이 아니다.");
+		}
+
+	}
+
+}
