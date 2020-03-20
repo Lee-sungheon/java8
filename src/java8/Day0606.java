e8625b84edcf6cd9b42045625b47c7e6a6727fe5
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-20 15:08:07 +0900

    6일차 : while문

---
+++ src/java8/Day0606.java
@@ -0,0 +1,22 @@
+package java8;
+
+public class Day0606 {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		while(i < 10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		i = 0;
+		while(i < 5)
+		{
+			System.out.println("Hello!");
+			i++;
+		}
+	}
+
+}
