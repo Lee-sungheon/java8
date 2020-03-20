887813a9fb34be031835c43e136af26e00810382
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-20 14:12:28 +0900

    6일차 : 중첩 for문

---
+++ src/java8/Day0603.java
@@ -0,0 +1,32 @@
+package java8;
+
+public class Day0603 {
+
+	public static void main(String[] args) {
+	
+		for(int i = 0 ; i < 10 ; i++)
+		{
+			System.out.println(i);
+		}
+		
+		for(int i = 0 ; i < 5; i++)
+		{
+			System.out.println("Hello");
+		}
+
+		for(int i=0 ; i<3 ; i++)
+		{
+			for(int j=0 ; j<5; j++)
+			{
+				System.out.print("¡Ú");
+			}
+			System.out.println("");
+		}
+		
+		for(int i=0 ; i<3 ; i++)
+		{
+			System.out.println("¾È³çÇÏ¼¼¿ä.");
+		}
+	}
+
+}
