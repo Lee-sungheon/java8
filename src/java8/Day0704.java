9be49c2813b3358caf6a5b074f8a86e6d8f023e7
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-23 09:45:42 +0900

    7일차 : 연습문제(4)

---
+++ src/java8/Day0704.java
@@ -0,0 +1,19 @@
+package java8;
+
+public class Day0704 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0, max = 10;
+		
+		for(int i = 1 ; i <= max ; i++)
+		{
+			if(i%2 == 0)
+				sum += i;
+		}
+		
+		System.out.println("1부터 " + max + "까지 짝수의 합 : " + sum);
+
+	}
+
+}
