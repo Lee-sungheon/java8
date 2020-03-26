9b041feed72bbf26220b4bef4a9603146633a56a
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-26 13:15:54 +0900

    10일차 : 배열(6)

---
+++ src/java8/Day1006.java
@@ -0,0 +1,31 @@
+package java8;
+
+public class Day1006 {
+
+	public static void main(String[] args) {
+		int arr1[] = new int[3];
+		for(int i=0 ; i<arr1.length ; i++) {
+			System.out.println(arr1[i]);
+		}
+		System.out.println();
+		
+		arr1[0] = 10;
+		arr1[1] = 20;
+		arr1[2] = 30;
+		for(int i=0 ; i<arr1.length ; i++) {
+			System.out.println(arr1[i]);
+		}
+		System.out.println();
+		
+		double[] arr2 = new double[3];
+		arr2[0] = 1.1;
+		arr2[1] = 2.2;
+		arr2[2] = 3.3;
+		
+		for(int i=0 ; i<arr2.length ; i++) {
+			System.out.println(arr2[i]);
+		}
+		System.out.println();
+	}
+
+}
