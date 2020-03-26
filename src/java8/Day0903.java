f3e720739da82f91d5662ceaac9030c0fcca4987
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-26 09:21:35 +0900

    9일차 : 연습문제(3)

---
+++ src/java8/Day0903.java
@@ -0,0 +1,14 @@
+package java8;
+
+public class Day0903 {
+
+	public static void main(String[] args) {
+		for(int i=65 ; i<65+26; i++)
+			System.out.printf("%c",i);
+		
+		System.out.println();
+		
+		for(int i=97 ; i<97+26; i++)
+			System.out.printf("%c",i);
+	}
+}
