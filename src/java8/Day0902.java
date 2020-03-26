f70081cfaa2d72882009fb227851e956328c2722
Leesungheon <Leesungheon.dev@gmail.com>
2020-03-26 09:04:28 +0900

    9일차 : 연습문제(2)

---
+++ src/java8/Day0902.java
@@ -0,0 +1,40 @@
+package java8;
+
+public class Day0902 {
+
+	public static void main(String[] args) {
+		
+		int cnt=0;
+		for(int i=0 ; i<5 ; i++)
+		{
+			if (i % 2 == 1) {
+				for (int j = 0; j < 5; j++)
+					System.out.print(cnt + 5 - j + "\t");
+				cnt += 5;
+			}
+			
+			else {
+				for (int j = 0; j < 5; j++)
+				{
+					cnt++;
+					System.out.print(cnt + "\t");
+				}
+			}
+
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i=0 ; i<5 ; i++)
+		{
+			for(int j=1 ; j<=i; j++)
+				System.out.print(" ");
+			System.out.print("@");
+			System.out.println();
+		}
+
+
+	}
+
+}
